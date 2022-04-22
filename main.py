f97f67e039716874ca72f01776f052ce422a7d28
Orie Steele <orie@transmute.industries>
2022-04-22 15:26:45 -0500

    :rocket:

--- main.py
+++ main.py
@@ -1,37 +1,16 @@
 import os
 import sys
 
-from yamllint import linter
-from yamllint.config import YamlLintConfig
-
-
 def main():
-    print(os.environ)
-    yaml_path = os.environ["INPUT_PATH"]
-    strict = os.environ["INPUT_STRICT"] == "true"
-    conf = YamlLintConfig("extends: default")
-    warning_count = 0
+  
+    input_path = os.environ["INPUT_PATH"]
 
-    with open(yaml_path) as f:
-        problems = linter.run(f, conf, yaml_path)
+    print(input_path)
 
-    for problem in problems:
+    message = '1'
+  
 
-        if problem.level == "warning" and strict:
-            problem.level = "error"
-
-        print(
-            f"::{problem.level} file={yaml_path},line={problem.line},"
-            f"col={problem.column}::{problem.desc} ({problem.rule})"
-        )
-
-        if problem.level == "warning":
-            warning_count = warning_count + 1
-
-        if problem.level == "error":
-            sys.exit(1)
-
-    print(f"::set-output name=warnings::{warning_count}")
+    print(f"::set-output name=warnings::{message}")
 
     sys.exit(0)
 
